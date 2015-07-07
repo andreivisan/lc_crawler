a9c119b51dbd4be46dd2d14a4ee7330cf134c65b
Andrei Visan <andrei.m.visan@gmail.com>
2015-07-07 20:47:48 +0200

    Added Gulp task to convert jsx files into js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-// place code for your default task here
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+
+gulp.task('js', function(){
+    browserify('./public/javascripts/src/app.jsx')
+        .transform(reactify)
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('public/javascripts/build/'));
 });
+
+gulp.task('default', ['js']);
---
+++ public/javascripts/src/app.jsx
@@ -0,0 +1,7 @@
+var React = require('react');
+var LoginForm = require('./LoginForm.jsx');
+
+React.render(
+    <LoginForm />,
+    document.getElementById('loginFormContainer')
+);
